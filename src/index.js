6bb18f36bfeb03e9641736b022c2b67497dc618f
mellowmorgan <morgan.waites@gmail.com>
2021-11-29 11:30:05 -0800

    add getElements method to UI logic
    Co-authored-by: Frank Proulx <frankproulx7@gmail.com>

--- src/index.js
+++ src/index.js
@@ -5,21 +5,28 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 
 function getElements(response) {
-  if (response.main) {
-    $('.showHumidity').text(`The humidity in ${response.name} is ${response.main.humidity}%`);
-    $('.showTemp').text(`The temperature in Kelvins is ${response.main.temp} degrees.`);
+  if (response.results) {
+    if (response.results.length>0){  
+      for(let i = 0; i < 5; i++) {
+        console.log(response.results[i].title);
+      }
+    } else {
+      console.log("No results found.");
+    }
   } else {
-    $('.showErrors').text(`There was an error: ${response.status_message}`);
+    console.log(`Error: ${response.status_message}`);
   }
 }
 
+// https://image.tmdb.org/t/p/original (movie poster link)
 
-let genres = [];
-genres = $("input[name='genres']:checked").map(function() {
-  return $(this).val();
-}).toArray();
+//let genres = [];
+// genres = $("input[name='genres']:checked").map(function() {
+//   return $(this).val();
+// }).toArray();
+let genres= ['16','80','27'];
 let genresString = genres.join(',');
 GenreFinder.makeGenreCall(genresString)
-.then(function(response) {
-  getElements(response);
-});
+  .then(function(response) {
+    getElements(response);
+  });
