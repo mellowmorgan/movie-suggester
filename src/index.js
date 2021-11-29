b92c1e0057bc85fe16ec760355ad6036dcb8b879
Katie Pundt <kzpundt@gmail.com>
2021-11-29 11:29:02 -0800

    add jQuery to get select values and generate buttons
    Co-authored-by: Aaron Minnick <abminnick@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-// import $ from 'jquery';
+import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
@@ -11,3 +11,19 @@ import './css/styles.css';
 // })
 
 // then the reverse to remove a chosen item if they decide not to use it
+
+$("#add-genre").on("click", () => {
+  let option = $("#genre :selected");
+  if (option.val() !== "" && option.prop("disabled") === false) {
+    $("#selected-genres").append(`
+      <button class="btn btn-success tag-btn" type="button" value="${option.val()}"">${option.text()} <span class="tag-btn-x">X</span></button>
+    `);
+    option.prop("disabled", true);
+  }
+});
+
+$("#selected-genres").on("click", "button", function() {
+  let button = $(this);
+  $(`#genre option[value=${button.val()}]`).prop("disabled", false);
+  button.remove();
+});
