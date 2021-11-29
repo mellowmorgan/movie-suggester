0ee5fde910f429ea995cf528aedf01b1abec8fb1
Katie Pundt <kzpundt@gmail.com>
2021-11-29 10:42:47 -0800

    set up project environment and add basic form
    Co-authored-by: Aaron Minnick <abminnick@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,4 +1,13 @@
-import $ from 'jquery';
+// import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+
+
+// say we have genres to choose id='choose-genre' and chosen 'chosen-genre'
+// $('choose-genre').on('click', 'checkbox', () => {
+//     this.hide
+//     chosen-genre.append
+// })
+
+// then the reverse to remove a chosen item if they decide not to use it
