73399d56fe4de61ca19277e1437a595e7eeadf98
mellowmorgan <morgan.waites@gmail.com>
2021-11-29 10:53:45 -0800

    add genreFinder method
    Co-authored-by: Frank Proulx <frankproulx7@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,11 +1,25 @@
 import $ from 'jquery';
-import ''
+import GenreFinder from './js/genreFinder.js';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+
+function getElements(response) {
+  if (response.main) {
+    $('.showHumidity').text(`The humidity in ${response.name} is ${response.main.humidity}%`);
+    $('.showTemp').text(`The temperature in Kelvins is ${response.main.temp} degrees.`);
+  } else {
+    $('.showErrors').text(`There was an error: ${response.status_message}`);
+  }
+}
 
 
 let genres = [];
 genres = $("input[name='genres']:checked").map(function() {
   return $(this).val();
 }).toArray();
+let genresString = genres.join(',');
+GenreFinder.makeGenreCall(genresString)
+.then(function(response) {
+  getElements(response);
+});
--- src/js/genreFinder.js
+++ src/js/genreFinder.js
@@ -1,8 +1,14 @@
-import { ids } from "webpack"
-
 export default class GenreFinder{
-  static async makeGenreCall(){
-    const response = await fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${process.env.API_KEY}&language=en-US&sort_by=popularity.desc&include_video=false&page=1&with_genres=878,28&with_original_language=en`);
-    
+  static makeGenreCall(stringIDs){
+    return fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${process.env.API_KEY}&language=en-US&sort_by=popularity.desc&include_video=false&page=1&with_genres=${stringIDs}&with_original_language=en`)
+    .then(function(response) {
+      if (!response.ok) {
+        throw Error(response.status);
+      }
+      return response.json();
+    }) 
+    .catch(function(error) {
+      return error.message;
+    });
   }
 }
