b966489c5b6ca14ad3c611432afd666e17e3199d
Katie Pundt <kzpundt@gmail.com>
2021-11-29 16:12:05 -0800

    fix jQuery to generate keyword suggestions
    Co-authored-by: Aaron Minnick <abminnick@gmail.com>

--- src/index.js
+++ src/index.js
@@ -40,21 +40,110 @@ $("#add-keyword").on("click", () => {
   if (keyword.val() !== "") {
     if ($("#add-keyword").css("--first-click") === "true") {
       //API CALL
-      let json = $.getJSON("./test.json", function(json) {
-        console.log(json);
-      });
+
+      let json = {
+        "page": 1,
+        "results": [
+          {
+            "id": 9951,
+            "name": "alien"
+          },
+          {
+            "id": 4939,
+            "name": "alien phenomenons"
+          },
+          {
+            "id": 10158,
+            "name": "alien planet"
+          },
+          {
+            "id": 14909,
+            "name": "alien invasion"
+          },
+          {
+            "id": 15250,
+            "name": "alien infection"
+          },
+          {
+            "id": 12553,
+            "name": "alien abduction"
+          },
+          {
+            "id": 160515,
+            "name": "alien contact"
+          },
+          {
+            "id": 163488,
+            "name": "human alien"
+          },
+          {
+            "id": 162459,
+            "name": "alien language"
+          },
+          {
+            "id": 163252,
+            "name": "alien race"
+          },
+          {
+            "id": 163386,
+            "name": "alien possession"
+          },
+          {
+            "id": 183787,
+            "name": "alien monster"
+          },
+          {
+            "id": 190042,
+            "name": "alien world"
+          },
+          {
+            "id": 193907,
+            "name": "evil alien"
+          },
+          {
+            "id": 218016,
+            "name": "alien autopsy"
+          },
+          {
+            "id": 220392,
+            "name": "alien parasites"
+          },
+          {
+            "id": 197194,
+            "name": "alien friendship"
+          },
+          {
+            "id": 200099,
+            "name": "space alien"
+          },
+          {
+            "id": 206281,
+            "name": "alien fugitive"
+          },
+          {
+            "id": 209033,
+            "name": "alien baby"
+          }
+        ],
+        "total_pages": 2,
+        "total_results": 39
+      };
       makeKeywordSuggestions(json);
+      $("#keyword-suggestions").slideToggle();
       $("#add-keyword").css("--first-click", "false");
     } else {
-      $("#keyword-suggestions :checked").each(function(element) {
+      console.log($("#add-keyword").css("--first-click"));
+      console.log($("#keyword-suggestions :checked"));
+      $("#keyword-suggestions :checked").each(function() {
         $("#selected-keywords").append(`
-        <button class="btn btn-success tag-btn" type="button" value="${element.val()}">${element.parent().text} <span class="tag-btn-x">X</span></button>
+        <button class="btn btn-success tag-btn" type="button" value="${$(this).val()}">${$(this).parent().text()} <span class="tag-btn-x">X</span></button>
         `);
       });
       $("#add-keyword").css("--first-click", "true");
+      $("#keyword-suggestions").html("");
+      $("#keyword-suggestions").toggle();
+      keyword.val("");
     }
-    
-    keyword.val("");
   }
 });
 
@@ -73,12 +162,6 @@ function makeKeywordSuggestions(json) {
     `);
   }
 }
-
-
-/* <label class="list-group-item">
-<input class="form-check-input me-1" type="checkbox" value="">
-First checkbox
-</label> */
 
 function getMovies(response) {
   if (response.results) {
